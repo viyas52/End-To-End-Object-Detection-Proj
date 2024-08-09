d46cfd1bc03ed13e53f7d2941466b8185d9d5f09
viyas52 <vyas.illusionist@gmail.com>
2024-08-09 18:17:23 +0530

    exception added

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from SignLang.logger import logging
+from SignLang.exception import CustomException
+import sys
 
 logging.info("welcome to the project")
+
+try:
+    a = 7/0
+    
+except Exception as e:
+    raise CustomException(e,sys)
